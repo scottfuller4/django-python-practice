e856c2170a4fba884358f19fb1b9b3b84ca33344
Scott <scottfuller4@gmail.com>
2019-01-17 15:24:07 -0500

    added jobs model and hooked it up to postgres db

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# you usually have one model per app - but you can have more
+
+
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
