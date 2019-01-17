8a7c99f03c306aa13c4397126e4cd2c395112459
Scott <scottfuller4@gmail.com>
2019-01-17 15:55:05 -0500

    added blog model, set up initial html doc

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# add the model to the settings
+
+# Create a migration
+# go into the terminal and enter python manage.py makemigrations
+
+# Migrate
+# terminal: python manage.py migrate
+
+# Add to the admin
